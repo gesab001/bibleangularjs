64670f007de6298b1fcb1da94968dc23dc584b0b
gesab001 <144000giovanni@gmail.com>
2020-05-10 05:38:58 +1200

    added function to get xml bible

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,10 +11,11 @@ app.config(['$routeProvider', function($routeProvider) {
 }])
 
 .controller('View1Ctrl', function($scope, $http, $interval) {
+    $scope.xmlBibles = {"Bible_Cebuano": "xml"};
     $scope.booklist = {"Genesis": "1", "Exodus": "2", "Leviticus": "3", "Numbers": "4", "Deuteronomy": "5", "Joshua": "6", "Judges": "7", "Ruth": "8", "1 Samuel": "9", "2 Samuel": "10", "1 Kings": "11", "2 Kings": "12", "1 Chronicles": "13", "2 Chronicles": "14", "Ezra": "15", "Nehemiah": "16", "Esther": "17", "Job": "18", "Psalms": "19", "Proverbs": "20", "Ecclesiastes": "21", "Song of Solomon": "22", "Isaiah": "23", "Jeremiah": "24", "Lamentations": "25", "Ezekiel": "26", "Daniel": "27", "Hosea": "28", "Joel": "29", "Amos": "30", "Obadiah": "31", "Jonah": "32", "Micah": "33", "Nahum": "34", "Habakkuk": "35", "Zephaniah": "36", "Haggai": "37", "Zechariah": "38", "Malachi": "39", "Matthew": "40", "Mark": "41", "Luke": "42", "John": "43", "Acts": "44", "Romans": "45", "1 Corinthians": "46", "2 Corinthians": "47", "Galatians": "48", "Ephesians": "49", "Philippians": "50", "Colossians": "51", "1 Thessalonians": "52", "2 Thessalonians": "53", "1 Timothy": "54", "2 Timothy": "55", "Titus": "56", "Philemon": "57", "Hebrews": "58", "James": "59", "1 Peter": "60", "2 Peter": "61", "1 John": "62", "2 John": "63", "3 John": "64", "Jude": "65", "Revelation": "66"};
     $scope.firstName = "John";
     $scope.code = "text";
-    $scope.versions = {"choices":[{"version": "Greek", "code": "text"}, {"version": "Tagalog", "code":"tagalog"}]};
+    $scope.versions = {"choices":[{"version": "KJV", "code": "kjv"}, {"version": "Cebuano", "code": "Bible_Cebuano"}, {"version": "Basic English", "code": "basicenglish"}, {"version": "Maori", "code": "maori"}, {"version": "Korean", "code": "korean"}, {"version": "Hebrew", "code": "hebrew"}, {"version": "Greek", "code": "textusreceptus"}, {"version": "Tagalog", "code":"tagalog"}]};
     $http.get("view1/topics.json")
     .then(function(response) {
         
@@ -27,23 +28,67 @@ app.config(['$routeProvider', function($routeProvider) {
         $scope.id = getCurrentID()
     }, 1000);
     $scope.changeVersion = function(code, bookName, chapterNumber, verseNumber, verseID, index) {
-        $scope.word = code;
         $scope.bookNumber = $scope.booklist[bookName];
-        $scope.url = getURL(code);
-        $http({method: "GET", url: $scope.url}).
-          then(function(response) {
-            $scope.topics.topics[index]["verses"][verseID].word = response.data["version"][$scope.bookNumber]["book"][chapterNumber]["chapter"][verseNumber]["verse"];
-            $scope.data = response.data;
-          }, function(response) {
-            $scope.word = response.data || 'Request failed';
-        });
+        
+        if (code in $scope.xmlBibles){
+            $scope.url = getXmlURL(code);
+            $scope.topics.topics[index]["verses"][verseID].word = getXmlURL(code);
+            
+            $http({method: "GET", url: $scope.url}).
+              then(function(response) {
+//                  
+                $scope.wordtext = getXmlVerse(response, $scope.bookNumber-1, chapterNumber-1, verseNumber-1);
+                $scope.topics.topics[index]["verses"][verseID].word = $scope.wordtext;
+              }, function(response) {
+                $scope.topics.topics[index]["verses"][verseID].word = response.data || 'Request failed';
+            });
+        }else{
+            $scope.url = getJsonURL(code, $scope.bookNumber);
+            $http({method: "GET", url: $scope.url}).
+              then(function(response) {
+                $scope.topics.topics[index]["verses"][verseID].word = response.data["version"][$scope.bookNumber]["book"][chapterNumber]["chapter"][verseNumber]["verse"];
+                $scope.data = response.data;
+              }, function(response) {
+                $scope.word = response.data || 'Request failed';
+            });
+        }
+        
       };
 
 }); 
 
-function getURL(code){
-    var code = "view1/versions/tagalog-version.json";
-    return code;
+function getXmlURL(code){
+    var url = "view1/versions/"+code+".xml";
+    return url;
+}
+
+function getXmlVerse(text, bookNumber, chapterNumber, verseNumber){ 
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(text.data,"text/xml");
+//    var bookNumber = 1;
+//    var chapterNumber = 1;
+//    var verseNumber = 1;
+    var books = xmlDoc.getElementsByTagName("BIBLEBOOK");
+    var chapters = books[bookNumber].getElementsByTagName("CHAPTER");
+    var verses = chapters[chapterNumber].getElementsByTagName("VERS");
+    var word = verses[verseNumber].childNodes[0].nodeValue;
+    return word;
+}
+
+function getJsonURL(code, bookNumber){
+    var url = "";
+
+    if(code=="textusreceptus" && bookNumber<40){
+        url = "view1/versions/greekot-version.json";
+    }else{
+        url = "view1/versions/"+code+"-version.json";
+    }
+    if(code=="hebrew" && bookNumber>39){
+        url = "view1/versions/hebrewmodern-version.json";
+    }else{
+        url = "view1/versions/"+code+"-version.json";
+    }
+    return url;
 }
 
 
